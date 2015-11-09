6a3356e0b2c51e6614bca710c01e6c82d9cda770
Markku Laine <markku.laine@aalto.fi>
2015-11-09 23:51:24 +0200

    Second version. Run with "npm run tsc" and "npm start"

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,7 +2,7 @@ import { Component, bootstrap } from 'angular2/angular2';
 
 @Component({
   selector: 'my-app',
-  template: '<h1>My First Angular 2 App</h1>'
+  template: '<h1>My SECOND Angular 2 App</h1>'
 })
 class AppComponent { }
 
